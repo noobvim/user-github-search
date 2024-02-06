6e655de8b39bbcd8adfbf463495c3e83bece633f
Christopher Myers <christopher.myers@walmart.com>
2024-02-05 17:00:27 -0700

    Clean up form styles.

--- app/ui/search-form.tsx
+++ app/ui/search-form.tsx
@@ -24,7 +24,7 @@ export function SearchForm({ setUser }: Props) {
 
   return (
     <div className="w-full area-bg-2 p-2 rounded-xl mt-4">
-      <form action={formAction} className="relative pl-20" id="form">
+      <form action={formAction} className="relative pl-20 flex space-between">
         <div className="absolute inset-y-0 start-0 flex items-center ps-6 pointer-events-none">
           <Image
             className="text-blue-50"
@@ -35,13 +35,13 @@ export function SearchForm({ setUser }: Props) {
           />
         </div>
         <input
-          className="area-bg-2 w-full text-lg"
+          className="area-bg-2 w-full text-lg !outline-none"
           name="username"
           placeholder="Search GitHub username..."
           type="text"
         />
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 border border-blue-700 rounded-xl"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 border border-blue-700 rounded-xl ml-2"
           type="submit"
         >
           Search
